4aa7c8ce6c217ad6ca5775d8aebfe60b36da855b
zankanmehta <zankanmehta@gmail.com>
2023-03-10 17:49:55 +0530

    jQuery

---
+++ jquery/3 removeclass.js
@@ -0,0 +1,7 @@
+$('#addClass').click(function(){
+    $('#box').addClass('bg color')
+})
+
+$('#removeClass').click(function(){
+    $('#box').removeClass('bg color')
+})
---
+++ jquery/4 toggleclass pra.js
@@ -0,0 +1,22 @@
+// $('#addclass').click(function(){
+//     $('#box').addclass('bg color')
+// })
+
+// $('#removeclass').click(function(){
+//     $('#box').removeclass('bg color')
+// })
+
+// $('#toggleclass').click(function(){
+//     $('#box').toggleclass('bg color')
+// })
+
+
+ $('#addclass').click(function () {
+            $('#box').addClass('bg color')
+        })
+        $('#removeclass').click(function () {
+            $('#box').removeClass('bg color')
+        })
+        $('#toggleclass').click(function () {
+            $('#box').toggleClass('bg color')
+        })
