dad8b8917e3433f214739f943d1e05203faf4272
zankanmehta <zankanmehta@gmail.com>
2023-03-11 18:19:53 +0530

    jQuery

---
+++ jquery/5 has pra.js
@@ -0,0 +1,13 @@
+$('#hasClass').click(function () {
+    ($('#box').hasClass('b'));
+
+    let x = $('#box').hasClass('b')
+
+    if (x == true) {
+        console.log('yaahhhhhhhh');
+    }
+    else {
+        console.log('noooooooo');
+
+    }
+});
---
+++ jquery/6 after before pra.js
@@ -0,0 +1,9 @@
+$('#after').click(function(){
+    $('#box').after('<H2> Mehta </H2>')
+})
+
+
+
+$('#before').click(function(){
+    $('#box').before('<h2> zankan </h2>')
+})
