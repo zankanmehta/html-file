12a7463c6d060f50520319e6a30757b35a37b863
zankanmehta <zankanmehta@gmail.com>
2023-03-24 11:10:49 +0530

    jQuery

---
+++ jquery/10 innerheight.js
@@ -0,0 +1,9 @@
+$( "div" ).one( "click", function() {
+    // console.log($( this ).height(50));
+    
+    $( this ).height( 5 ).css({
+      cursor: "auto",
+      backgroundColor: "green"
+    });
+  });
+  
